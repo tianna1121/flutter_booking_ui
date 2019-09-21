b17c4902e5a89e1bb356de45f00ce15d6f922aa2
yang zhang <tianna1121@gmail.com>
2019-09-21 22:22:52 +0800

    implement the home page with icon_card, image_card and image_cards widgets

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,11 +1,164 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_booking_ui/widgets/icon_card.dart';
+import 'package:flutter_booking_ui/widgets/image_cards.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('HomePage'),
+      body: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.menu),
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.person_outline),
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Hello, ',
+                          style: TextStyle(
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.pinkAccent),
+                        ),
+                        TextSpan(
+                          text: 'what are you looking for?',
+                          style: TextStyle(
+                              fontSize: 32.0,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  IconCard(
+                    iconData: Icons.home,
+                    text: 'Accomodation',
+                  ),
+                  IconCard(
+                    iconData: Icons.directions_bike,
+                    text: 'Experience',
+                  ),
+                  IconCard(
+                    iconData: Icons.directions,
+                    text: 'Advantures',
+                  ),
+                  IconCard(
+                    iconData: Icons.flight,
+                    text: 'Flights',
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                      'Best Experient',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.more_horiz,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              ImageCards(),
+              SizedBox(
+                height: 25.0,
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(
+                        Icons.home,
+                        color: Colors.pinkAccent,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.search,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.favorite_border,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.person_outline,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
